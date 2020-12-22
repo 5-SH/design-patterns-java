6a70792a9a3112f71057d03f587381f67644403b
OSH <llll_lll_lll@naver.com>
2020-12-23 00:26:11 +0900

    iterator 추가

--- src/iterator/Waitress.java
+++ src/iterator/Waitress.java
@@ -5,20 +5,25 @@ import java.util.Iterator;
 public class Waitress {
   Menu pancakeHouseMenu;
   Menu dinerMenu;
+  Menu cafeMenu;
 
-  public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+  public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
     this.pancakeHouseMenu = pancakeHouseMenu;
     this.dinerMenu = dinerMenu;
+    this.cafeMenu = cafeMenu;
   }
 
   public void printMenu() {
     Iterator pancakeIterator = pancakeHouseMenu.createIterator();
     Iterator dinerIterator = dinerMenu.createIterator();
+    Iterator cafeIterator = cafeMenu.createIterator();
 
     System.out.println("메뉴\n----\n아침식사");
     printMenu(pancakeIterator);
     System.out.println("\n저녁 식사");
     printMenu(dinerIterator);
+    System.out.println("\n카페 메뉴");
+    printMenu(cafeIterator);
   }
 
   public void printMenu(Iterator iterator) {
