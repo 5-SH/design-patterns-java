f260f381aaaffe19df5dddbf9be7c58a8a8f68cd
OSH <llll_lll_lll@naver.com>
2020-12-29 22:30:23 +0900

    state 패턴 추가

---
+++ src/state/GumballMachine.java
@@ -0,0 +1,79 @@
+package state;
+
+public class GumballMachine {
+  State soldOutState;
+  State noQuarterState;
+  State hasQuarterState;
+  State soldState;
+  State winnerState;
+
+  State state = soldOutState;
+  int count = 0;
+  
+  public GumballMachine(int numberGumballs) {
+    this.soldOutState = new SoldOutState(this);
+    this.noQuarterState = new NoQuarterState(this);
+    this.hasQuarterState = new HasQuarterState(this);
+    this.soldState = new SoldState(this);
+    this.winnerState = new WinnerState(this);
+
+    this.count = numberGumballs;
+    if (numberGumballs > 0) {
+      state = noQuarterState;
+    }
+  }
+
+  public void insertQuarter() {
+    state.insertQuarter();
+  }
+
+  public void ejectQuarter() {
+    state.ejectQuater();
+  }
+
+  public void turnCrank() {
+    state.turnCrank();
+    state.dispense();
+  }
+
+  public void setState(State state) {
+    this.state = state;
+  }
+
+  public void releaseBall() {
+    System.out.println("알맹이가 나가고 있습니다.");
+    if (count != 0) {
+      count = count - 1;
+    }
+  }
+
+  public int getCount() {
+    return count;
+  }
+
+  public State getSoldOutState() {
+    return soldOutState;
+  }
+
+  public State getNoQuarterState() {
+    return noQuarterState;
+  }
+
+  public State getHasQuarterState() {
+    return hasQuarterState;
+  }
+
+  public State getSoldState() {
+    return soldState;
+  }
+
+  public State getWinnerState() {
+    return winnerState;
+  }
+
+  @Override
+  public String toString() {
+    return "주식회사 왕뽑기\n자바로 돌아가는 뽑기 기계\n남은 개수: " + count + "개\n" +
+            (state.getClass().getName() == "state.NoQuarterState" ? "동전 투입 대기중" : "매진") + "\n";
+  }
+}
